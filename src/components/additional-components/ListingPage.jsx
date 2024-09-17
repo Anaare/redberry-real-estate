011a711239750d18ce2dc33e0dac7d8fc4129f90
Anaare <arevadzeana10@gmail.com>
2024-09-17 17:48:02 +0400

    add listing page

--- src/components/additional-components/ListingPage.jsx
+++ src/components/additional-components/ListingPage.jsx
@@ -10,9 +10,40 @@ function ListingPage() {
 					<img src="./images/property-images/property-1.png" alt="property-1" />
 					<div className={styles.description}>
 						<h1>80, 458 ₾</h1>
+						<div className={styles.details}>
+							<div className={styles.info}>
+								<img
+									src="./images/icons/location-marker.svg"
+									alt="location icon"
+								/>
+								<span>თბილისი, ი. ჭავჭავაძის 53</span>
+							</div>
+							<div className={styles.info}>
+								<img src="./images/icons/area.svg" alt="area icon" />
+								<span>ფართი 55 მ²</span>
+							</div>
+							<div className={styles.info}>
+								<img src="./images/icons/bed.svg" alt="bed icon" />
+								<span>საძინებელი 2</span>
+							</div>
+							<div className={styles.info}>
+								<img src="./images/icons/zip-code.svg" alt="zip code icon" />
+								<span>საფოსტო ინდექსი 2525</span>
+							</div>
+							<p>
+								იყიდება ბინა ჭავჭავაძის ქუჩაზე, ვაკეში. ბინა არის ახალი
+								რემონტით, ორი საძინებლით და დიდი აივნით. მოწყობილია ავეჯითა და
+								ტექნიკით.
+							</p>
+						</div>
+						{/*დასამატებელია აგენტის ინფო და წაშლის ღილაკი + სტილებია დასაკორექტირებელი.
+                        
+                            ასევე! არ დამავიწყდეს მოდალი (წაშლის ღილაკზე დაჭერისას გამოდის)
+                        */}
 					</div>
 				</div>
 			</div>
+			<p className={styles.releasedInfo}>გამოქვეყნების თარიღი 08/08/2024</p>
 		</div>
 	);
 }
